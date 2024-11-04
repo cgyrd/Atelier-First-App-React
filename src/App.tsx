901479793db0c40172a74b82084d75f7796dbe3f
CAMILLEG <cgyrd@outlook.fr>
2024-11-04 15:04:37 +0100

    Pokemon quête 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import PokemonCard from './components/PokemonCard';
+import React from "react";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <div>
-   <PokemonCard pokemon={pokemonList[0]} />
-   </div>
-)
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
+  );
 }
 
 const pokemonList = [
   {
-      name: "Bulbasaur le BG",
-      imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    name: "Bulbasaur le BG",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
-  
-  {
-      name: "Mew"  
-  }
-];
 
+  {
+    name: "Mew",
+  },
+];
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-
-
-function PokemonCard(props) {
-
-    const { pokemon } = props;
-       return (
-        <figure>
-         {pokemon.imgSrc ? (
-           <img
-           src={pokemon.imgSrc}
-           alt={pokemon.name}
-       />
-   ) : (
-       <p>???</p>
-   )}
-            <figcaption>
-                <h1>{pokemon.name}</h1>
-            </figcaption>
-        </figure>
-    );
+import React from "react";
+interface Pokemon {
+  name: string;
+  imgSrc?: string;
 }
 
+interface PokemonCardProps {
+  pokemon: Pokemon;
+}
+
+function PokemonCard(props: PokemonCardProps) {
+  const { pokemon } = props;
+
+  console.log(pokemon);
+
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>
+        <h1>{pokemon.name}</h1>
+      </figcaption>
+    </figure>
+  );
+}
 
 export default PokemonCard;
