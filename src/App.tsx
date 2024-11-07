78a7e06c695c6a8155804087e443c85432f60d4b
CAMILLEG <cgyrd@outlook.fr>
2024-11-07 19:33:49 +0100

    création d'un state PokemonIndex

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [PokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[PokemonIndex]} />
     </div>
   );
 }
@@ -15,7 +22,21 @@ const pokemonList = [
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
-
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
   {
     name: "Mew",
   },
